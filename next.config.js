95de2117c70de565034a94d8d6ce8b127be84df4
Paul Sastrasinh <psastras@gmail.com>
2022-01-20 01:25:14 +0900

    fix: next.js paths

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: process.env.GITHUB_ACTIONS ? "/rust-component-tracker" : "",
+  trailingSlash: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
