75f10511a1967d6c64d9a14d35fe922aa10cf785
psastras <psastras@users.noreply.github.com>
2023-02-27 01:32:30 +0000

    deploy: 22636de4e4cac691d474da5a7c19f9659fa7abb1

---
+++ _next/static/TyeGGqQzxrPyzJbEgZzd_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/619-a2efb874ad03fbc0.js","static/chunks/pages/index-1f469c77509daf2a.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
